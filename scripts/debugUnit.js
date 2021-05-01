42d70a3f5a0f5cf66cf9b2aa2b1a6c3b0a85b716
2593828650 <2593828650@qq.com>
2021-05-01 10:49:51 +0800

    update

--- scripts/debugUnit.js
+++ scripts/debugUnit.js
@@ -2,3 +2,5 @@ const a11 = extendContent(UnitType, 'debug-core-unit', {});
 a11.abilities.add(new ForceFieldAbility(250, 1500, 50000, 30), new RepairFieldAbility(1500, 30, 250));
 a11.abilities.add(new ShieldRegenFieldAbility(1500, 5000, 60, 250));
 a11.constructor = prov(() => extend(UnitTypes.mega.constructor.get().class, {}));
+a11.immunities = ObjectSet.with(StatusEffects.slow, StatusEffects.unmoving);
+UnitTypes.oct.defaultController = () => new FlyingAI();
